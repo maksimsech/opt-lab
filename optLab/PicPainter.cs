5372196583afab659f2aec89e0eb73be06d38bd5
maksimsech <maksim.sech@gmail.com>
2019-05-18 20:35:01 +0300

    Retarger to .Net 4.7.2 Edit ToPic function

--- optLab/PicPainter.cs
+++ optLab/PicPainter.cs
@@ -30,46 +30,46 @@ namespace optLab
         {
             var firstCoordinates = ToPic(picWidth, picHeight, offset, botBorder, upBorder, firstX2, firstX1);
             var secondCoordinates = ToPic(picWidth, picHeight, offset, botBorder, upBorder, secondX2, secondX1);
-            picGraphics.DrawLine(pen, firstCoordinates.Item1, firstCoordinates.Item2, secondCoordinates.Item1, secondCoordinates.Item2);
+            picGraphics.DrawLine(pen, firstCoordinates.x, firstCoordinates.y, secondCoordinates.x, secondCoordinates.y);
         }
 
         public void DrawCoordinateLines(double botBorder, double upBorder, double offset,int size, double deltaX)
         {
             var font = new Font("Times New Roman", CalcFontSize(deltaX));
             var coordinates = ToPic(picWidth, picHeight, offset, botBorder, upBorder, 0d, 0d);
-            picGraphics.DrawLine(Pens.Black, coordinates.Item1, 0, coordinates.Item1, origHeight);
-            picGraphics.DrawLine(Pens.Black, 0, coordinates.Item2, origWidth, coordinates.Item2);
-            picGraphics.DrawString("x1", font, Brushes.Black, coordinates.Item1, 0);
-            picGraphics.DrawString("x2", font, Brushes.Black, origWidth - font.Size * 2, coordinates.Item2 - font.Size * 2);
-            picGraphics.DrawString("0", font, Brushes.Black, coordinates.Item1 - font.Size * 2, coordinates.Item2);
-            var deltaPlus = coordinates.Item2;
+            picGraphics.DrawLine(Pens.Black, coordinates.x, 0, coordinates.x, origHeight);
+            picGraphics.DrawLine(Pens.Black, 0, coordinates.y, origWidth, coordinates.y);
+            picGraphics.DrawString("x1", font, Brushes.Black, coordinates.x, 0);
+            picGraphics.DrawString("x2", font, Brushes.Black, origWidth - font.Size * 2, coordinates.y - font.Size * 2);
+            picGraphics.DrawString("0", font, Brushes.Black, coordinates.x - font.Size * 2, coordinates.y);
+            var deltaPlus = coordinates.y;
             var deltaCoordinates = ToPic(picWidth, picHeight, 0, botBorder, upBorder, deltaX, deltaX);
             for(var i = deltaX; i <= upBorder; i += deltaX)
             {
                 var value = Math.Round(i, 2);
-                deltaPlus -= deltaCoordinates.Item1;
-                picGraphics.DrawString(value.ToString(), font, Brushes.Black, coordinates.Item1 - font.Size * 2, deltaPlus);             
+                deltaPlus -= deltaCoordinates.x;
+                picGraphics.DrawString(value.ToString(), font, Brushes.Black, coordinates.x - font.Size * 2, deltaPlus);             
             }
-            deltaPlus = coordinates.Item2;
+            deltaPlus = coordinates.y;
             for (var i = -deltaX; i >= botBorder; i -= deltaX)
             {
                 var value = Math.Round(i, 2);
-                deltaPlus += deltaCoordinates.Item1;
-                picGraphics.DrawString(value.ToString(), font, Brushes.Black, coordinates.Item1 - font.Size * 2, deltaPlus);
+                deltaPlus += deltaCoordinates.x;
+                picGraphics.DrawString(value.ToString(), font, Brushes.Black, coordinates.x - font.Size * 2, deltaPlus);
             }
-            deltaPlus = coordinates.Item1;
+            deltaPlus = coordinates.x;
             for (var i = deltaX; i <= upBorder; i += deltaX)
             {
                 var value = Math.Round(i, 2);
-                deltaPlus += deltaCoordinates.Item1;
-                picGraphics.DrawString(value.ToString(), font, Brushes.Black, deltaPlus - font.Size, coordinates.Item2);
+                deltaPlus += deltaCoordinates.x;
+                picGraphics.DrawString(value.ToString(), font, Brushes.Black, deltaPlus - font.Size, coordinates.y);
             }
-            deltaPlus = coordinates.Item1;
+            deltaPlus = coordinates.x;
             for (var i = -deltaX; i >= botBorder; i -= deltaX)
             {
                 var value = Math.Round(i, 2);
-                deltaPlus -= deltaCoordinates.Item1;
-                picGraphics.DrawString(value.ToString(), font, Brushes.Black, deltaPlus - font.Size, coordinates.Item2);
+                deltaPlus -= deltaCoordinates.x;
+                picGraphics.DrawString(value.ToString(), font, Brushes.Black, deltaPlus - font.Size, coordinates.y);
             }
         }
 
@@ -82,9 +82,9 @@ namespace optLab
             {
                 brush = Brushes.Green;
                 size = minPointSize;
-                picGraphics.DrawString("min", new Font("Times New Roman", 8f), Brushes.Green, coordinates.Item1, coordinates.Item2);
+                picGraphics.DrawString("min", new Font("Times New Roman", 8f), Brushes.Green, coordinates.x, coordinates.y);
             }
-            picGraphics.FillRectangle(brush, coordinates.Item1, coordinates.Item2, size, size);           
+            picGraphics.FillRectangle(brush, coordinates.x, coordinates.y, size, size);           
         }
 
         /// <summary>
@@ -98,12 +98,12 @@ namespace optLab
         /// <param name="x2"></param>
         /// <param name="x1"></param>
         /// <returns></returns>
-        private Tuple<float, float> ToPic(double width, double height, double offset, double bot, double up, double x2, double x1)
+        private (float x, float y) ToPic(double width, double height, double offset, double bot, double up, double x2, double x1)
         {
             var size = Math.Abs(bot) + Math.Abs(up);
             var x = width / size * x2 + width * offset; 
             var y = height - (height / size * x1 + height * offset) + 0.125d* height;
-            return new Tuple<float, float>((float)x, (float)y);
+            return ((float) x, (float)y);
         }
 
         private float CalcFontSize(double step) => (float)(10d * step / 0.5);
